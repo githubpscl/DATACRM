269c02d290431f3929c5177346dcc9cd0991a701
pascal <pascalmnagel@gmail.com>
2025-07-31 15:46:06 +0200

    Neues Datenvormat

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  // Add any additional props if needed
+  // Additional props can be added here when needed
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
